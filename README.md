12cb901383ba22f3e2d7e8cdf75b7ed24d145796
fabiocat93 <fabio-cat93@hotmail.it>
2023-05-11 21:02:41 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Fab - Voice Analysis Package
+
+Welcome to Fab - a voice analysis package that aims to help you analyze and visualize audio recordings of human speech. This package is currently in progress, so please expect some bugs and inconsistencies.
+
+## Installation
+
+To install Fab, simply run the following command in your terminal:
+
+'''
+source myscript.sh
+'''
+
+
+This command will create a Conda environment called `fab`, clone all the required repositories, install all dependencies, and activate the `fab` environment. Once the command has finished running, you should see a message indicating that the `fab` environment is now active and ready to use.
+
+## Quick Start
+
+TODO
+
+## Contact
+
+If you have any questions, comments, or suggestions, please feel free to reach out to Fabio Catania at fabiocat@mit.edu. We appreciate your feedback and will do our best to address any issues you encounter.
---
+++ setup.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+#################### config #########################
+env_name="fab"
+tools_folder="tools" 
+conda_sh="/Users/fabiocat/miniconda3/etc/profile.d/conda.sh"
+#################### config #########################
+
+######################################## start #############################################
+
+echo "Hello, $env_name!"
+
+if conda info --envs | grep -q "/$env_name$"; then
+  echo "Environment '$env_name' already exists"
+else
+  echo "Environment '$env_name' does not exist"
+  conda create -n $env_name python=3.8 -y
+fi
+
+source $conda_sh
+conda activate $env_name
+echo "Active Conda environment: $CONDA_DEFAULT_ENV"
+
+if [ -d "$tools_folder" ]; then
+  echo "Folder '$tools_folder' already exists"
+else
+  echo "Folder '$tools_folder' does not exist"
+  mkdir $tools_folder
+fi
+
+cd $tools_folder
+
+#################### pyannote-audio #########################
+repo_url="https://github.com/fabiocat93/pyannote-audio.git"
+repo_dir="pyannote-audio"
+
+if [ -d "$repo_dir" ]; then
+  # Repository exists, pull updates
+  echo "Repository '$repo_dir' exists, pulling updates..."
+  cd "$repo_dir"
+  git pull
+  cd .. 
+else
+  # Repository does not exist, clone it
+  echo "Repository '$repo_dir' does not exist, cloning..."
+  git clone "$repo_url" "$repo_dir"
+fi
+
+cd "$repo_dir"
+pip install -e .[dev,testing]
+pre-commit install
+cd ..
+#################### pyannote-audio #########################
+
+#################### serab-byols #########################
+repo_url="https://github.com/GasserElbanna/serab-byols.git"
+repo_dir="serab-byols"
+
+if [ -d "$repo_dir" ]; then
+  # Repository exists, pull updates
+  echo "Repository '$repo_dir' exists, pulling updates..."
+  cd "$repo_dir"
+  git pull
+  cd .. 
+else
+  # Repository does not exist, clone it
+  echo "Repository '$repo_dir' does not exist, cloning..."
+  git clone "$repo_url" "$repo_dir"
+fi
+
+cd "$repo_dir"
+pip install -e .
+cd ..
+#################### serab-byols #########################
+
+#################### s3prl #########################
+repo_url="https://github.com/s3prl/s3prl.git"
+repo_dir="s3prl"
+
+if [ -d "$repo_dir" ]; then
+  # Repository exists, pull updates
+  echo "Repository '$repo_dir' exists, pulling updates..."
+  cd "$repo_dir"
+  git pull
+  cd .. 
+else
+  # Repository does not exist, clone it
+  echo "Repository '$repo_dir' does not exist, cloning..."
+  git clone "$repo_url" "$repo_dir"
+fi
+
+cd "$repo_dir"
+pip install -e ".[all]"
+cd ..
+#################### s3prl #########################
+
+#################### fab #########################
+pip install -r requirements.txt
+cd ../..
+#################### fab #########################
+
+echo "$env_name is ready to go!"
+
+######################################## end #############################################
+
+
