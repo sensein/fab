046636754070e83ccc502eaa884a278c5cdbad1b
fabiocat93 <fabio-cat93@hotmail.it>
2023-05-15 14:04:14 -0400

    slurm test #5

--- children-voice-detection/slurm-pipeline/pipeline.sh
+++ children-voice-detection/slurm-pipeline/pipeline.sh
@@ -11,16 +11,12 @@ fi
 #SBATCH -t 1:00:00          # walltime = 1 hours and 30 minutes
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=fabiocat@mit.edu
+# Set the array variable based on the calculated array size
+#SBATCH --array=1-12 # TODO: adjust this value based on len(ENCODER)*len(DECODER)
 
 # Define your lists of encoders and decoders
 ENCODERS=("encoder1" "encoder2" "encoder3" "encoder4")
 DECODERS=("decoder1" "decoder2" "decoder3")
-
-# Calculate the array size based on the length of encoders and decoders
-ARRAY_SIZE=$(( ${#ENCODERS[@]} * ${#DECODERS[@]} ))
-
-# Set the array variable based on the calculated array size
-#SBATCH --array=1-$ARRAY_SIZE
 
 # Calculate the current encoder and decoder based on the array index
 ENCODER_INDEX=$(((SLURM_ARRAY_TASK_ID - 1) / ${#DECODERS[@]}))
