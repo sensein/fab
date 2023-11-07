f16e22b0a5ad05309719ad1811ee01d4b4d2f4c9
fabiocat93 <fabio-cat93@hotmail.it>
2023-11-07 09:49:57 -0500

    projects folder removed

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 #################### config #########################
-env_name="fab"
-conda_sh="/om2/user/fabiocat/myconda/etc/profile.d/conda.sh"
+env_name="fab" # change this, if you really want :'(
+conda_sh="/om2/user/fabiocat/myconda/etc/profile.d/conda.sh" # change this based on your path to conda
 #################### config #########################
